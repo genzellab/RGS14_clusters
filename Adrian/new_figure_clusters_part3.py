06186a8be784f204260242aad19a209488ae84d5
Aleman-Z <jraleman10@gmail.com>
2024-10-15 08:38:38 +0200

    Update new_figure_clusters_part3.py

--- Adrian/new_figure_clusters_part3.py
+++ Adrian/new_figure_clusters_part3.py
@@ -10,7 +10,31 @@ from scipy.stats import gaussian_kde
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
+import os
+from scipy.io import loadmat
 
+os.chdir('/home/adrian/Downloads')
+
+# Load the .mat file
+data = loadmat('mydata_pca_veh.mat')
+#data = loadmat('mydata_pca_rgs.mat')
+
+# Extract variables from the loaded data
+x_axis1 = data['x_axis1']
+y_axis1 = data['y_axis1']
+z_axis1 = data['z_axis1']
+x_axis2 = data['x_axis2']
+y_axis2 = data['y_axis2']
+z_axis2 = data['z_axis2']
+x_axis3 = data['x_axis3']
+y_axis3 = data['y_axis3']
+z_axis3 = data['z_axis3']
+x_axis4 = data['x_axis4']
+y_axis4 = data['y_axis4']
+z_axis4 = data['z_axis4']
+
+os.chdir('/home/adrian/Documents/GitHub/UMAP/utils/')
+#%%
 
 # Remove NaNs for all clusters
 #Cluster 3
@@ -35,12 +59,12 @@ z_axis4 = z_axis4[~np.isnan(z_axis4)]
 #ax = fig.add_subplot(111, projection='3d')
 
 # # Set up figure and create 1x2 subplot (1 row, 2 columns)
-# fig = plt.figure(figsize=(15, 8))
-# ax = fig.add_subplot(121, projection='3d')  # First subplot
+#fig = plt.figure(figsize=(15, 8))
+#ax = fig.add_subplot(121, projection='3d')  # First subplot
 # ax2 = fig.add_subplot(122, projection='3d')  # Second subplot
 # Set up figure and create 2x2 subplot (2 rows, 2 columns)
-fig = plt.figure(figsize=(15, 15))
 
+fig = plt.figure(figsize=(15, 15))
 # Create 4 subplots in a 2x2 grid
 ax = fig.add_subplot(221, projection='3d')  # First subplot
 ax2 = fig.add_subplot(222, projection='3d')  # Second subplot
@@ -71,7 +95,7 @@ def plot_cluster_noKDE(x, y, z, ax):
     #kde = gaussian_kde(np.vstack([x, y]))
     
     # Plot 3D scatter
-    sc = ax.scatter(x, y, z, color='gray', alpha=0.1, rasterized=True)
+    sc = ax.scatter(x, y, z, color='gray', alpha=0.05, rasterized=True)
         #scatter = ax.scatter(x, y, z, c=feature, alpha=alpha, s=s, color=color)
     return sc
 
@@ -81,10 +105,13 @@ np.random.seed(42)  # You can choose any integer value
 # Randomly sample 16000 unique indices
 indices = np.random.choice(len(x_axis4), size=16000, replace=False)
 
-# Use the sampled indices to create downsampled arrays
-x_downsampled = x_axis4[indices]
-y_downsampled = y_axis4[indices]
-z_downsampled = z_axis4[indices]
+# # Use the sampled indices to create downsampled arrays
+#x_downsampled = x_axis4[indices]
+#y_downsampled = y_axis4[indices]
+#z_downsampled = z_axis4[indices]
+x_downsampled = x_axis4
+y_downsampled = y_axis4
+z_downsampled = z_axis4
 
 # # Plot all clusters on the same figure
 # sc1 = plot_cluster(x_axis1, y_axis1, z_axis1, custom_cmps[0], ax)
@@ -100,9 +127,8 @@ sc3 = plot_cluster(x_axis3, y_axis3, z_axis3, cmaps[1], ax) # Cluster 2
 #sc4 = plot_cluster(x_axis4, y_axis4, z_axis4, cmaps[3], ax) # Cluster 1
 sc4 = plot_cluster(x_downsampled, y_downsampled, z_downsampled, cmaps[2], ax)
 
-
 # Customize plot
-ax.set_title('3D Kernel Density Estimation for Clusters',fontsize=12)
+ax.set_title('VEH (Clusters)',fontsize=12)
 ax.set_xlabel('PCA1')
 ax.set_ylabel('PCA2')
 ax.set_zlabel('PCA3')
@@ -111,7 +137,7 @@ ax.set_zlabel('PCA3')
 #ax.set_ylim(-10,10)
 ###ax.set_zlim(0,10)
 ax.set_xlim(-10,35)
-ax.set_ylim(-10,10)
+ax.set_ylim(-10,12)
 ax.set_zlim(-7.770710858935466, 20)
 
 
@@ -145,49 +171,47 @@ ax.set_zticks([0, 10, 20])           # For the Z-axis
 
 plt.draw()  # Update the plot with new ticks
 
-# Plot all clusters on ax2 (same as ax)
-sc1_2 = plot_cluster(x_axis1, y_axis1, z_axis1, cmaps[0], ax2)  # Cluster 3
-sc2_2 = plot_cluster_noKDE(x_axis2, y_axis2, z_axis2, ax2)  # Cluster 4
-sc3_2 = plot_cluster(x_axis3, y_axis3, z_axis3, cmaps[1], ax2)  # Cluster 2
-sc4_2 = plot_cluster(x_downsampled, y_downsampled, z_downsampled, cmaps[2], ax2)  # Cluster 1
+#%%
+# Combine clusters 1,2 and 3. Amount of ripples per cluster is similar.
+x = np.concatenate((x_axis1, x_axis3,x_downsampled), axis=0)
+y = np.concatenate((y_axis1, y_axis3,y_downsampled), axis=0)
+z = np.concatenate((z_axis1, z_axis3,z_downsampled), axis=0)
 
-# Customize ax2
-ax2.set_title('3D Kernel Density Estimation for Clusters (No Grid)',fontsize=12)
-ax2.set_xlabel('PCA1')
-ax2.set_ylabel('PCA2')
-ax2.set_zlabel('PCA3')
+cmaps = ['inferno'] #, 'plasma', 'inferno',plasma cividis
 
-# Set limits for ax2
+# Plot clusters on the third subplot (ax3)
+sc9 = plot_cluster(x, y, z, cmaps[0], ax2)  # Cluster 3
+sc10 = plot_cluster_noKDE(x_axis2, y_axis2, z_axis2, ax2)  # Cluster 4
+
+# Customize ax3 plot
+ax2.set_title('VEH (All ripples)',fontsize=12)  # Title for ax3
+ax2.set_xlabel('PCA1', fontsize=10)
+ax2.set_ylabel('PCA2', fontsize=10)
+ax2.set_zlabel('PCA3', fontsize=10)
+
+# Set axis limits for ax3
 ax2.set_xlim(-10, 35)
-ax2.set_ylim(-10, 10)
+ax2.set_ylim(-10, 12)
 ax2.set_zlim(-7.770710858935466, 20)
 
-# Add colorbars for each cluster on ax2
-fig.colorbar(sc1_2, ax=ax2, label='KDE Cluster 3')
-fig.colorbar(sc3_2, ax=ax2, label='KDE Cluster 2')
-fig.colorbar(sc4_2, ax=ax2, label='KDE Cluster 1')
+# Add colorbar for the first cluster in ax3
+fig.colorbar(sc9, ax=ax2, label='KDE All ripples')  # Add colorbar for sc1
+fig.colorbar(sc9, ax=ax2, label='KDE All ripples')  # Add colorbar for sc1
+fig.colorbar(sc9, ax=ax2, label='KDE All ripples')  # Add colorbar for sc1
 
+# Set view angle for ax3
 ax2.view_init(azim=180, elev=20)
 
-# Set font size for axis labels in ax2
-ax2.set_xlabel("PCA1", fontsize=10)
-ax2.set_ylabel("PCA2", fontsize=10)
-ax2.set_zlabel("PCA3", fontsize=10)
+# Optional: Set ticks and grid
+ax2.tick_params(axis='x', labelsize=10)  # X-axis tick font size
+ax2.tick_params(axis='y', labelsize=10)  # Y-axis tick font size
+ax2.tick_params(axis='z', labelsize=10)  # Z-axis tick font size
+ax2.grid(True)  # Enable grid for ax3
 
-# Set tick parameters for ax2
-ax2.tick_params(axis='x', labelsize=10)
-ax2.tick_params(axis='y', labelsize=10)
-ax2.tick_params(axis='z', labelsize=10)
+ax2.set_xticks([-10, 0, 10, 20, 30])  # For the X-axis
+ax2.set_yticks([-10, -5, 0, 5, 10])   # For the Y-axis
+ax2.set_zticks([0, 10, 20])           # For the Z-axis
 
-# Set custom ticks for each axis in ax2
-ax2.set_xticks([-10, 0, 10, 20, 30])
-ax2.set_yticks([-10, -5, 0, 5, 10])
-ax2.set_zticks([0, 10, 20])
-
-# Turn off grid for ax2
-ax2.grid(False)
-
-plt.draw()  # Update the plot with new ticks
 
 #%%
 ax = plt.gca()  # Get current axes;
@@ -217,16 +241,17 @@ print(f"Z-axis tick label font size: {z_tick_fontsize}")
 #%%
 
 # Generate and save frames
-for angle in range(0, 360, 18):  # Rotate every 2 degrees
-    ax.view_init(azim=angle)
+for angle in range(0, 360, 10):  # Rotate every 2 degrees
+    ax.view_init(azim=angle,elev=20)
     plt.savefig(f"frame_{angle}.png", dpi=300)  # Save each frame
 
 plt.close()
+#%%
 
 from moviepy.editor import ImageSequenceClip
 
 # List of all the frame files
-frames = [f"frame_{angle}.png" for angle in range(0, 360, 18)]
+frames = [f"frame_{angle}.png" for angle in range(0, 360, 10)]
 
 # Create a video clip
 clip = ImageSequenceClip(frames, fps=5)
